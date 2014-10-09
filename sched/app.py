32f81d31fded946c3e0d048aeb604fcf3eecddac
Ramon Octavio Badillo Rivas <robr8@hotmail.com>
2014-10-08 23:45:37 -0500

    rutas added

--- sched/app.py
+++ sched/app.py
@@ -1,10 +1,39 @@
 from flask import Flask
 
 app = Flask(__name__)
-@app.route('/')
 
+
+@app.route('/')
 def hello():
-	return 'Hello, world!'
+    return 'Hello, world!'
 
 if __name__ == '__main__':
-	app.run(debug = True)
+    app.run(debug=True)
+
+
+@app.route('/appointments/')
+def appointment_list():
+    return 'Listing of all appointments we have.'
+
+
+@app.route('/appointments/<int:appointment_id>/')
+def appointment_detail(appointment_id):
+    return 'Detail of appointment #{}.'.format(appointment_id)
+
+
+@app.route(
+    '/appointments/<int:appointment_id>/edit/',
+    methods=['GET', 'POST'])
+
+
+@app.route(
+    '/appointments/create/',
+    methods=['GET', 'POST'])
+def appointment_create():
+    return 'Form to create a new appointment.'
+
+
+@app.route('/ appointments / <int: appointment_id > /delete /,
+           methods=['DELETE'])
+def appointment_delete(appointment_id):
+    raise NotImplementedError('DELETE')
