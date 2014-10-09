6e33d0e372a2b73bac3ee29e2a094d53a5b3a609
Ramon Octavio Badillo Rivas <robr8@hotmail.com>
2014-10-09 18:59:35 -0500

    dia 2

--- sched/app.py
+++ sched/app.py
@@ -1,7 +1,7 @@
-
 from flask import Flask
+from flask import url_for
 from flask.ext.sqlalchemy import SQLAlchemy
-from sched.models import Base
+from models import Base
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///sched.db'
@@ -12,7 +12,6 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///sched.db'
 db = SQLAlchemy(app)
 db.Model = Base
 
-
 @app.route('/appointments/')
 def appointment_list():
     return 'Listing of all appointments we have.'
@@ -20,16 +19,10 @@ def appointment_list():
 
 @app.route('/appointments/<int:appointment_id>/')
 def appointment_detail(appointment_id):
-    return 'Detail of appointment #{}.'.format(appointment_id)
-
-
-@app.route(
-    '/appointments/<int:appointment_id>/edit/',
-    methods=['GET', 'POST'])
-@app.route('/appointments/<int:appointment_id>/edit/', methods=['GET', 'POST'])
-def appointment_edit(appointment_id):
-    return 'Form to edit appointment #.'.format(appointment_id)
-
+ edit_url = url_for('appointment_edit',
+ appointment_id=appointment_id)
+ # Return the URL string just for demonstration.
+ return edit_url
 
 @app.route(
     '/appointments/create/',
@@ -38,10 +31,10 @@ def appointment_create():
     return 'Form to create a new appointment.'
 
 
-@app.route('/appointments/<int:appointment_id>/delete/', methods=['DELETE'])
+@app.route(
+    '/appointments/<int:appointment_id>/delete/', methods=['DELETE'])
 def appointment_delete(appointment_id):
-    raise NotImplementedError('DELETE')
-
+	return 'borrar'
 
 if __name__ == '__main__':
-    manager.run()
+	app.run()
