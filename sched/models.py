e0068e4110e45dca39dbbcca3fab722f98f944e9
Ramon Octavio Badillo Rivas <robr8@hotmail.com>
2014-10-09 20:54:04 -0500

    base arreglada

--- sched/models.py
+++ sched/models.py
@@ -3,7 +3,7 @@ from sqlalchemy import Boolean, Column
 from sqlalchemy import DateTime, Integer, String, Text
 from sqlalchemy.ext.declarative import declarative_base
 
-from sqlalchemy.orm import synonym
+
 
 
 
