9faf49e6fca9d27a11f8f45ab1a03cfb3997e3ea
Ramon Octavio Badillo Rivas <robr8@hotmail.com>
2014-10-09 19:13:10 -0500

    templates base added

--- sched/models.py
+++ sched/models.py
@@ -7,7 +7,6 @@ Base = declarative_base()
 
 class Appointment(Base):
 
-    """An appointment on the calendar."""
     __tablename__ = 'appointment'
 
     id = Column(Integer, primary_key=True)
@@ -28,72 +27,85 @@ class Appointment(Base):
     def __repr__(self):
         return ('<{self.__class__.__name__}: {self.id}>'.format(self=self))
 
-if __name__ == '__main__':
+
+if __name__ == '__main__':  # pragma: no cover
     from datetime import timedelta
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-engine = create_engine('sqlite:///sched.db', echo=True)
+    from sqlalchemy import create_engine
+    from sqlalchemy.orm import sessionmaker
 
-Base.metadata.create_all(engine)
-Session = sessionmaker(bind=engine)
-session = Session()
+    engine = create_engine('sqlite:///sched.db', echo=True)
 
-now = datetime.now()
-session.add(Appointment(
-    title='Important Meeting',
-    start=now + timedelta(days=3),
-    end=now + timedelta(days=3, seconds=3600),
-    allday=False,
-    location='The Office'))
-session.commit()
-session.add(Appointment(
-    title='Past Meeting',
-    start=now - timedelta(days=3),
-    end=now - timedelta(days=3, seconds=3600),
-    allday=False,
-    location='The Office'))
-session.commit()
-session.add(Appointment(
-    title='Follow Up',
-    start=now + timedelta(days=4),
-    end=now + timedelta(days=4, seconds=3600),
-    allday=False,
-    location='The Office'))
-session.commit()
-session.add(Appointment(
-    title='Day Off',
-    start=now + timedelta(days=5),
-    end=now + timedelta(days=5),
-    allday=True))
-session.commit()
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
 
-# Create. Add a new model instance to the session.
-appt = Appointment(
-    title='My Appointment',
-    start=now,
-    end=now + timedelta(seconds=1800),
-    allday=False)
-session.add(appt)
-session.commit()
-# Update. Update the object in place, then commit.
-appt.title = 'Your Appointment'
-session.commit()
-# Delete. Tell the session to delete the object.
-session.delete(appt)
-session.commit()
+    now = datetime.now()
 
-# Get an appointment by ID.
-appt = session.query(Appointment).get(1)
-# Get all appointments.
-appts = session.query(Appointment).all()
-# Get all appointments before right now, after right now.
-appts = session.query(Appointment).filter(Appointment.start <
-                                          datetime.now()).all()
-appts = session.query(Appointment).filter(Appointment.start >=
-                                          datetime.now()).all()
-# Get all appointments before a certain date.
-appts = session.query(Appointment).filter(Appointment.start <=
-                                          datetime(2013, 5, 1)).all()
-# Get the first appointment matching the filter query.
-appt = session.query(Appointment).filter(Appointment.start <=
-                                         datetime(2013, 5, 1)).first()
+    session.add(Appointment(
+        title='Important Meeting',
+        start=now + timedelta(days=3),
+        end=now + timedelta(days=3, seconds=3600),
+        allday=False,
+        location='The Office'))
+    session.commit()
+
+    session.add(Appointment(
+        title='Past Meeting',
+        start=now - timedelta(days=3),
+        end=now - timedelta(days=3, seconds=3600),
+        allday=False,
+        location='The Office'))
+    session.commit()
+
+    session.add(Appointment(
+        title='Follow Up',
+        start=now + timedelta(days=4),
+        end=now + timedelta(days=4, seconds=3600),
+        allday=False,
+        location='The Office'))
+    session.commit()
+
+    session.add(Appointment(
+        title='Day Off',
+        start=now + timedelta(days=5),
+        end=now + timedelta(days=5),
+        allday=True))
+    session.commit()
+
+    # Create. Add a new model instance to the session.
+    appt = Appointment(
+        title='My Appointment',
+        start=now,
+        end=now + timedelta(seconds=1800),
+        allday=False)
+
+    session.add(appt)
+    session.commit()
+
+    # Update. Update the object in place, then commit.
+    appt.title = 'Your Appointment'
+    session.commit()
+
+    # Delete. Tell the session to delete the object.
+    session.delete(appt)
+    session.commit()
+
+    # Get an appointment by ID.
+    appt = session.query(Appointment).get(1)
+
+    # Get all appointments.
+    appts = session.query(Appointment).all()
+
+    # Get all appointments before right now, after right now.
+    appts = session.query(Appointment).filter(
+        Appointment.start < datetime.now()).all()
+    appts = session.query(Appointment).filter(
+        Appointment.start >= datetime.now()).all()
+
+    # Get all appointments before a certain date.
+    appts = session.query(Appointment).filter(
+        Appointment.start <= datetime(2013, 5, 1)).all()
+
+    # Get the first appointment matching the filter query.
+    appt = session.query(Appointment).filter(
+        Appointment.start <= datetime(2013, 5, 1)).first()
