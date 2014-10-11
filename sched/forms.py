9d57d7e00370e45350fb5adf0c40203c41f8a557
Luis <luis.lopez@cimat.mx>
2014-10-11 13:19:16 -0500

    Se agrego el form de login

--- sched/forms.py
+++ sched/forms.py
@@ -1,5 +1,5 @@
 from wtforms import Form, BooleanField, DateTimeField
-from wtforms import TextAreaField, TextField
+from wtforms import TextAreaField, TextField, PasswordField
 from wtforms.validators import Length, required
 
 
@@ -10,3 +10,12 @@ class AppointmentForm(Form):
     allday = BooleanField('All Day')
     location = TextField('Location', [Length(max=255)])
     description = TextAreaField('Description')
+
+
+class LoginForm(Form):
+
+    """
+    From para el login de usuario
+    """
+    username = TextField('Email', [required()])
+    password = PasswordField('Password', [required()])
