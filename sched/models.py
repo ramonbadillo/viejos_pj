6c4d6837ac272e18a251f6909115b68100039261
Luis <luis.lopez@cimat.mx>
2014-10-11 13:45:48 -0500

    Se agrego el modelo del usuario

--- sched/models.py
+++ sched/models.py
@@ -1,15 +1,64 @@
 from datetime import datetime
-from sqlalchemy import Boolean, Column
+from sqlalchemy import Boolean, Column, ForeignKey
 from sqlalchemy import DateTime, Integer, String, Text
 from sqlalchemy.ext.declarative import declarative_base
-
-
-
-
-
-
+from sqlalchemy.orm import relationship, synonym
+from werkzeug import check_password_hash
+from werkzeug import generate_password_hash
 
 Base = declarative_base()
+
+
+class User(Base):
+
+    __tablename__ = 'user'
+    id = Column(Integer, primary_key=True)
+    created = Column(DateTime, default=datetime.now)
+    modified = Column(DateTime, default=datetime.now, onupdate=datetime.now)
+    name = Column('name', String(200))
+    email = Column(String(100), unique=True, nullable=False)
+    active = Column(Boolean, default=True)
+    _password = Column('password', String(100))
+
+    def _get_password(self):
+        return self._password
+
+    def _set_password(self, password):
+        if password:
+            password = password.strip()
+        self._password = generate_password_hash(password)
+        password_descriptor = property(self._get_password, self._set_password)
+        password = synonym('_password', descriptor=password_descriptor)
+
+    def check_password(self, password):
+        if self._password is None:
+            return False
+        password = password.strip()
+        if not password:
+            return False
+        return check_password_hash(self._password, password)
+
+    @classmethod
+    def authenticate(cls, query, email, password):
+        email = email.strip().lower()
+        user = query(cls).filter(cls.email == email).first()
+        if user is None:
+            return None, False
+        if not user.active:
+            return user, False
+        return user, user.check_password(password)
+
+    def get_id(self):
+        return str(self.id)
+
+    def is_active(self):
+        return True
+
+    def is_anonymous(self):
+        return False
+
+    def is_authenticated(self):
+        return True
 
 
 class Appointment(Base):
@@ -25,6 +74,9 @@ class Appointment(Base):
     allday = Column(Boolean, default=False)
     location = Column(String(255))
     description = Column(Text)
+
+    user_id = Column(Integer, ForeignKey('user_id'), nullable=False)
+    user = relationship(User, lazy='joined', join_depth=1)
 
     @property
     def duration(self):
