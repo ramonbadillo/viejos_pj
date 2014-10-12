ed0dccce13ea5254db3120992b2d2b4b90673495
Ramon Octavio Badillo Rivas <robr8@hotmail.com>
2014-10-11 23:51:37 -0500

    app arreglada

--- sched/app.py
+++ sched/app.py
@@ -1,13 +1,17 @@
-from flask import Flask
-from flask import abort, jsonify, redirect, render_template, request, url_for
+from flask import *
 from flask.ext.sqlalchemy import SQLAlchemy
+from flask.ext.login import LoginManager, current_user
+from flask.ext.login import login_user, logout_user, login_required
 
-from .forms import AppointmentForm
-from .models import Base, Appointment
-
+from models import Base
+from forms import AppointmentForm, LoginForm
+from models import Appointment
+import filters
+from models import User
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///sched.db'
+app.secret_key = 'secret_key'
 
 # Use Flask-SQLAlchemy for its engine and session
 # configuration. Load the extension, giving it the app object,
@@ -16,25 +20,72 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///sched.db'
 db = SQLAlchemy(app)
 db.Model = Base
 
+filters.init_app(app)
+
+
+# Use Flask-Login to track current user in Flask's session.
+login_manager = LoginManager()
+login_manager.setup_app(app)
+login_manager.login_view = 'login'
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    """Flask-Login hook to load a User instance from ID."""
+    return db.session.query(User).get(user_id)
+
+
+@app.route('/login/', methods=['GET', 'POST'])
+def login():
+    validarUsuario()
+
+    form = LoginForm(request.form)
+    error = None
+
+    if request.method == 'POST' and form.validate():
+        email = form.username.data.lower().strip()
+        password = form.password.data.lower().strip()
+        user, authenticated = User.authenticate(
+            db.session.query, email, password)
+
+        if authenticated:
+            login_user(user)
+            return redirect(url_for('appointment_list'))
+        else:
+            error = 'Incorrect username or password.'
+
+    return render_template('user/login.html', form=form, error=error)
+
+
+def validarUsuario():
+    if current_user.is_authenticated():
+        return redirect(url_for('appointment_list'))
+
+
+@app.route('/logout/')
+def logout():
+    logout_user()
+    return redirect(url_for('login'))
+
 
 @app.route('/appointments/')
+@login_required
 def appointment_list():
-    """Provide HTML listing of all appointments."""
+    """
+    Provide HTML listing of all appointments
+    """
     # Query: Get all Appointment objects, sorted by date.
     appts = (db.session.query(Appointment)
              .order_by(Appointment.start.asc()).all())
-    return render_template('appointment/index.html',
-                           appts=appts)
-
-
-@app.route('/')
-def index():
-    return render_template('index.html')
+    return render_template('appointment/index.html', appts=appts)
 
 
 @app.route('/appointments/<int:appointment_id>/')
+@login_required
 def appointment_detail(appointment_id):
-    """Provide HTML page with a given appointment."""
+    """
+    Muestra el detalle de una cita en especifico
+    """
     # Query: get Appointment object by ID.
     appt = db.session.query(Appointment).get(appointment_id)
     if appt is None:
@@ -43,24 +94,13 @@ def appointment_detail(appointment_id):
     return render_template('appointment/detail.html', appt=appt)
 
 
-@app.route('/appointments/create/', methods=['GET', 'POST'])
-def appointment_create():
-    """Provide HTML form to create a new appointment."""
-    form = AppointmentForm(request.form)
-    if request.method == 'POST' and form.validate():
-        appt = Appointment()
-        form.populate_obj(appt)
-        db.session.add(appt)
-        db.session.commit()
-        # Success. Send user back to full appointment list.
-        return redirect(url_for('appointment_list'))
-    # Either first load or validation error at this point.
-    return render_template('appointment/edit.html', form=form)
-
-
-@app.route('/appointments/<int:appointment_id>/edit/', methods=['GET', 'POST'])
+@app.route('/appointments/<int:appointment_id>/edit/',
+           methods=['GET', 'POST'])
+@login_required
 def appointment_edit(appointment_id):
-    """Provide HTML form to edit a given appointment."""
+    """
+    Provide HTML form to edit a given appointment
+    """
     appt = db.session.query(Appointment).get(appointment_id)
     if appt is None:
         abort(404)
@@ -70,13 +110,49 @@ def appointment_edit(appointment_id):
         db.session.commit()
         # Success. Send the user back to the detail view.
         return redirect(url_for('appointment_detail', appointment_id=appt.id))
+    # Either
     return render_template('appointment/edit.html', form=form)
 
 
-@app.route(
-    '/appointments/<int:appointment_id>/delete/', methods=['DELETE'])
+@app.route('/appointments/create/', methods=['GET', 'POST'])
+@login_required
+def appointment_create():
+    """
+    Provide HTML form to create a new appointment.
+    """
+    form = AppointmentForm(request.form)
+    if request.method == 'POST' and form.validate():
+
+        appt = Appointment(user_id=current_user.id)
+        if appt.user_id != current_user.id:
+            abort(403)
+
+        form.populate_obj(appt)
+        print("Start: {}".format(appt.start))
+
+        db.session.add(appt)
+        db.session.commit()
+        # Success. Send user back to full appointment list.
+        return redirect(url_for('appointment_list'))
+    # Either first load or validation error at this point.
+    return render_template('appointment/edit.html', form=form)
+
+
+@app.route('/appointments/<int:appointment_id>/delete/', methods=['DELETE'])
+@login_required
 def appointment_delete(appointment_id):
-    return 'borrar'
+    """Delete record using HTTP DELETE, respond with JSON."""
+    appt = db.session.query(Appointment).get(appointment_id)
+    if appt is None:
+        abort(404)
+    db.session.delete(appt)
+    db.session.commit()
+    return jsonify({'status': 'OK'})
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
 
 
 @app.errorhandler(404)
--- sched/models.py
+++ sched/models.py
@@ -1,8 +1,8 @@
 from datetime import datetime
 from sqlalchemy import Boolean, Column, ForeignKey
 from sqlalchemy import DateTime, Integer, String, Text
-from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship, synonym
+from sqlalchemy.ext.declarative import declarative_base
 from werkzeug import check_password_hash
 from werkzeug import generate_password_hash
 
@@ -11,6 +11,7 @@ Base = declarative_base()
 
 class User(Base):
 
+    """A user login, with credentials and authentication."""
     __tablename__ = 'user'
     id = Column(Integer, primary_key=True)
     created = Column(DateTime, default=datetime.now)
@@ -18,6 +19,7 @@ class User(Base):
     name = Column('name', String(200))
     email = Column(String(100), unique=True, nullable=False)
     active = Column(Boolean, default=True)
+
     _password = Column('password', String(100))
 
     def _get_password(self):
@@ -63,11 +65,14 @@ class User(Base):
 
 class Appointment(Base):
 
+    """An appointment on the calendar."""
     __tablename__ = 'appointment'
 
     id = Column(Integer, primary_key=True)
     created = Column(DateTime, default=datetime.now)
-    modified = Column(DateTime, default=datetime.now, onupdate=datetime.now)
+    modified = Column(DateTime, default=datetime.now,
+                      onupdate=datetime.now)
+
     title = Column(String(255))
     start = Column(DateTime, nullable=False)
     end = Column(DateTime, nullable=False)
@@ -75,7 +80,8 @@ class Appointment(Base):
     location = Column(String(255))
     description = Column(Text)
 
-    user_id = Column(Integer, ForeignKey('user_id'), nullable=False)
+    user_id = Column(Integer,
+                     ForeignKey('user.id'), nullable=False)
     user = relationship(User, lazy='joined', join_depth=1)
 
     @property
@@ -84,87 +90,31 @@ class Appointment(Base):
         return delta.days * 24 * 60 * 60 + delta.seconds
 
     def __repr__(self):
-        return ('<{self.__class__.__name__}: {self.id}>'.format(self=self))
-
-
-if __name__ == '__main__':  # pragma: no cover
+        return (u'<{self.__class__.__name__}: {self.id}>'.format(self=self))
+   
+if __name__ == "__main__":
     from datetime import timedelta
     from sqlalchemy import create_engine
     from sqlalchemy.orm import sessionmaker
-
+    
     engine = create_engine('sqlite:///sched.db', echo=True)
-
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
 
+#    Add user1
+    user = User(name="Usuario Nuevo", email="email@cimat.mx")
+    user._set_password("thepassword")
+    session.add(user)
+    session.commit()
+    
     now = datetime.now()
-
-    session.add(Appointment(
-        title='Important Meeting',
-        start=now + timedelta(days=3),
-        end=now + timedelta(days=3, seconds=3600),
-        allday=False,
-        location='The Office'))
-    session.commit()
-
-    session.add(Appointment(
-        title='Past Meeting',
-        start=now - timedelta(days=3),
-        end=now - timedelta(days=3, seconds=3600),
-        allday=False,
-        location='The Office'))
-    session.commit()
-
-    session.add(Appointment(
-        title='Follow Up',
-        start=now + timedelta(days=4),
-        end=now + timedelta(days=4, seconds=3600),
-        allday=False,
-        location='The Office'))
-    session.commit()
-
-    session.add(Appointment(
-        title='Day Off',
-        start=now + timedelta(days=5),
-        end=now + timedelta(days=5),
-        allday=True))
-    session.commit()
-
-    # Create. Add a new model instance to the session.
     appt = Appointment(
-        title='My Appointment',
-        start=now,
+        title='Otra cita con el dentista', start=now,
         end=now + timedelta(seconds=1800),
-        allday=False)
+        allday=False, location='asdas',description='asdasd',user_id=1)
 
     session.add(appt)
     session.commit()
+        
 
-    # Update. Update the object in place, then commit.
-    appt.title = 'Your Appointment'
-    session.commit()
-
-    # Delete. Tell the session to delete the object.
-    session.delete(appt)
-    session.commit()
-
-    # Get an appointment by ID.
-    appt = session.query(Appointment).get(1)
-
-    # Get all appointments.
-    appts = session.query(Appointment).all()
-
-    # Get all appointments before right now, after right now.
-    appts = session.query(Appointment).filter(
-        Appointment.start < datetime.now()).all()
-    appts = session.query(Appointment).filter(
-        Appointment.start >= datetime.now()).all()
-
-    # Get all appointments before a certain date.
-    appts = session.query(Appointment).filter(
-        Appointment.start <= datetime(2013, 5, 1)).all()
-
-    # Get the first appointment matching the filter query.
-    appt = session.query(Appointment).filter(
-        Appointment.start <= datetime(2013, 5, 1)).first()
